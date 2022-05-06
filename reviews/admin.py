7af3c2f507b69f0ade22b5f80b97d63eba81ea88
neil314159 <neil@neilboland.com>
2022-05-06 17:00:09 +0000

    Add admin site configuration

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
+from .models import Comment
+from .models import Category
 
-# Register your models here.
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Category)
+
+
--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return self.name
+
 class Review(models.Model):
     title = models.CharField(max_length=250, unique=True)
     author = models.ForeignKey(
@@ -11,7 +19,7 @@ class Review(models.Model):
     book_cover = CloudinaryField('image', default='placeholder')
     
 
-    # category = models.ForeignKey('Category', null=True, blank=True)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     review_text = models.TextField()
     published_on = models.DateTimeField(auto_now_add=True)
     star_rating = models.IntegerField()
@@ -44,9 +52,3 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment text is: {self.comment_text}"
 
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    
-
-    def __str__(self):
-        return self.name
