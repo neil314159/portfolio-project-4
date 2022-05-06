b66f0b879f8e7b7da4dcd0a8397c9bb8621a5525
neil314159 <neil@neilboland.com>
2022-05-06 17:25:40 +0000

    Add html templates to read out list of reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+class ReviewList(generic.ListView):
+    model = Review
+    queryset = Review.objects.order_by("-published_on")
+    template_name = "index.html"
