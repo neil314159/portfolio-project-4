ad2b69d68b44dc462ab88d0f45277fef24170496
neil314159 <neil@neilboland.com>
2022-05-06 16:33:17 +0000

    Create models for reviews, categories and comments

---
+++ reviews/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class Review(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
+    slug = models.SlugField(max_length=250, unique=True)
+    book_cover = CloudinaryField('image', default='placeholder')
+    
+
+    # category = models.ForeignKey('Category', null=True, blank=True)
+    review_text = models.TextField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    star_rating = models.IntegerField()
+    purchase_link = models.URLField(max_length=250)
+    
+
+
+    class Meta:
+        ordering = ["-published_on"]
+
+    def __str__(self):
+        return self.title
+
+   
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="author"
+    )
+   
+    comment_text = models.TextField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    
+
+    class Meta:
+        ordering = ["published_on"]
+
+    def __str__(self):
+        return f"Comment text is: {self.comment_text}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return self.name
