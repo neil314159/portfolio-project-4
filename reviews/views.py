9a2e16d408ed226a3150e755bd94a2609554b762
neil314159 <neil@neilboland.com>
2022-05-06 18:16:35 +0000

    Add review detail template and view for viewing individual reviews on site

--- reviews/views.py
+++ reviews/views.py
@@ -6,3 +6,21 @@ class ReviewList(generic.ListView):
     model = Review
     queryset = Review.objects.order_by("-published_on")
     template_name = "index.html"
+
+class ReviewDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.all
+        review = get_object_or_404(queryset, slug=slug)
+        
+       
+
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "post": post,
+                
+                
+            },
+        )
