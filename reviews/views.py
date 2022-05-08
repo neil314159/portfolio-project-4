9b324c1422417eacd07e9bb6bd661ab1abd1634b
neil314159 <neil@neilboland.com>
2022-05-08 12:42:16 +0000

    Alter views.py to simplyfy DetailView for vireing published reviews

--- reviews/views.py
+++ reviews/views.py
@@ -2,33 +2,37 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Review, Comment, Category
 
+
 class ReviewList(generic.ListView):
     model = Review
     queryset = Review.objects.order_by("-published_on")
     template_name = "index.html"
 
-class ReviewDetail(View):
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Review.objects.all()
-        review = get_object_or_404(queryset, slug=slug)
-        return render(
-            request,
-            "review_detail.html",
-            {
-                "review": review,
-            },
-        )
 
-    def post(self, request, slug, *args, **kwargs):
-        queryset = Review.objects.all()
-        review = get_object_or_404(queryset, slug=slug)
-        return render(
-            request,
-            "review_detail.html",
-            {
-                "review": review,
-            },
-        )
+class ReviewDetail(generic.DetailView):
+    model = Review
+    template_name = "review_detail.html"
+    # def get(self, request, slug, *args, **kwargs):
+    #     queryset = Review.objects.all()
+    #     review = get_object_or_404(queryset, slug=slug)
+    #     return render(
+    #         request,
+    #         "review_detail.html",
+    #         {
+    #             "review": review,
+    #         },
+    #     )
+
+    # def post(self, request, slug, *args, **kwargs):
+    #     queryset = Review.objects.all()
+    #     review = get_object_or_404(queryset, slug=slug)
+    #     return render(
+    #         request,
+    #         "review_detail.html",
+    #         {
+    #             "review": review,
+    #         },
+    #     )
 
 class ReviewCreateView(generic.CreateView):
     model = Review
