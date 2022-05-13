e4a19cd3a60c8b6f60e089663d398fb4faf73198
neil314159 <neil@neilboland.com>
2022-05-13 01:31:15 +0000

    Test new footer layout

--- bookbarn/settings.py
+++ bookbarn/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['book-barn-reviews.herokuapp.com', 'localhost']
 
@@ -123,21 +123,21 @@ WSGI_APPLICATION = 'bookbarn.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
-
-if DEBUG:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            os.environ.get("DATABASE_URL"))}
+
+# if DEBUG:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.parse(
+#             os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
