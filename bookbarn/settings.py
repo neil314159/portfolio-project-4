00d6efa2f4a132b94a84fb43c49d98a5aa0c6f02
neil314159 <neil@neilboland.com>
2022-05-13 00:13:53 +0000

    Add settings for local and remote databases to prepare for automated tests

--- bookbarn/settings.py
+++ bookbarn/settings.py
@@ -123,9 +123,21 @@ WSGI_APPLICATION = 'bookbarn.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
+if DEBUG:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
