2a82c8bf408bec8b79d9133b4b48b84858196d4d
neil314159 <neil@neilboland.com>
2022-05-15 04:27:23 +0000

    Test deploy CSS to heroku

--- bookbarn/settings.py
+++ bookbarn/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['book-barn-reviews.herokuapp.com', 'localhost']
 
@@ -137,17 +137,7 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-# if DEBUG:
-#     DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.parse(
-#             os.environ.get("DATABASE_URL"))}
+
 
 
 # Password validation
